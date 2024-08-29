e1b8763c87984268071a8cb95dc2056361ff5ed6
coffey-dev <scoffey@abc.gob.ar>
2024-08-29 12:16:56 -0300

    Devuelve todos los registros con FETCH

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  useEffect(() => {
+    const url = "https://prueba-gurudev.turso.io/v2/pipeline";
+    const authToken = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjQ5NDI5MjYsImlkIjoiMjMzMTYwOTktYzE3OS00MmNlLTk1NTAtMzZkY2M2ODIzNjMxIn0.l-aGn0jaL_NUbfnjnkzt9fmAOIR8nVDaKCGdwg8oa5HMe5x1ZsKDAg7nX2h9xhCwGN98uDuiAP242qcUxwfpAQ";
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        requests: [
+          { type: "execute", stmt: { sql: "SELECT * FROM mitabla" } }, // Asegúrate de que 'users' es el nombre correcto de tu tabla
+          { type: "close" },
+        ],
+      }),
+    })
+      .then((res) => res.text()) // Cambiar a .text() para ver la respuesta completa
+      .then((text) => {
+        console.log("Response text:", text); // Imprimir la respuesta completa
+        try {
+          const data = JSON.parse(text);
+          console.log(data);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
